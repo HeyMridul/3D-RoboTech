ffab2230d96f781ed1e21b8536cf0e931c4ec037
Mridul Bhardwaj <mridul.2005.05@gmail.com>
2026-08-23 22:54:32 +0530

    digi grid formed

---
+++ src/components/three/DigitalGrid.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import { Grid } from "@react-three/drei";
+
+interface DigitalGridProps {
+  size?: number;
+  divisions?: number;
+}
+
+export function DigitalGrid({ size = 30, divisions = 30 }: DigitalGridProps) {
+  return (
+    <Grid
+      args={[size, divisions]}
+      cellSize={1}
+      cellThickness={0.5}
+      cellColor="#1a1f28"
+      sectionSize={5}
+      sectionThickness={1}
+      sectionColor="#00d4ff33"
+      fadeDistance={25}
+      fadeStrength={1}
+      followCamera={false}
+      infiniteGrid
+      position={[0, -2, 0]}
+    />
+  );
+}
